d4644c5a3afd14f542e13736c1c34c05f6e2103b
Dean Richards <dprichards1981@googlemail.com>
2020-11-27 08:23:03 +0000

    add base data and images

---
+++ src/data/projects/index.js
@@ -0,0 +1,38 @@
+export const projects = {
+	socialApe: {
+		name: 'Social Ape',
+		imageName: 'thumb-gray.png',
+		blurb:
+			'A simple React website with Redux, that was writen useing class based components.  I converted it to functional components and updated redux to use the redux tool-kit',
+		link: 'http://dean-richards.uk',
+		featured: true,
+		category: ['mern', 'react', 'redux', 'firebase'],
+	},
+	iMessageClone: {
+		name: 'iMessage Clone',
+		imageName: 'imessage.jpg',
+		blurb:
+			'A basic iMessage clone that runs off firebase and uses the firebase authentication API. It uses firebase snapshots to update chats when a user adds a new message',
+		link: 'http://dean-richards.uk',
+		featured: true,
+		category: ['mern', 'react', 'redux', 'firebase'],
+	},
+
+	deanRichards: {
+		name: 'Dean Richards',
+		imageName: 'deanrichards.png',
+		blurb: 'Your looking at it!',
+		link: 'http://dean-richards.uk',
+		featured: true,
+		category: ['mern', 'react', 'redux', 'firebase'],
+	},
+	instagramClone: {
+		name: 'Instagram Clone',
+		imageName: 'deanrichards.png',
+		blurb:
+			'A basic Instagram clone that runs off firebase and uses the firebase authentication API. It uses firebase snapshots to update chats when a user adds a new message',
+		link: 'http://dean-richards.uk',
+		featured: true,
+		category: ['mern', 'react', 'redux', 'firebase'],
+	},
+};
