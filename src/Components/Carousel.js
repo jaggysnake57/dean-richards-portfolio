cd9463bae68e101a95b30febab2f14cbe48a2fed
Dean Richards <dprichards1981@googlemail.com>
2021-01-25 15:17:15 +0000

    add slide functionality to the carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,15 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { ProjectsContext } from '../contexts/ProjectsContext';
 import '../css/Components/Carousel/Main.css';
 import '../css/Components/Carousel/responsive.css';
 import { AiOutlineLeft } from 'react-icons/ai';
 import { AiOutlineRight } from 'react-icons/ai';
+import Whirligig from 'react-whirligig';
 
 const Carousel = ({ currentPage }) => {
 	const { projects, currentProject, setCurrentProject } = useContext(
 		ProjectsContext
 	);
+	const [windowSize, setWindowSize] = useState(0);
+	const [imageNumber, setImageNumber] = useState(1);
 
 	let history = useHistory();
 
@@ -29,13 +32,44 @@ const Carousel = ({ currentPage }) => {
 			}
 		}
 	};
+	const handleResize = (e) => {
+		setWindowSize(window.innerWidth);
+	};
+	useEffect(() => {
+		window.addEventListener('resize', handleResize);
+		setWindowSize(window.innerWidth);
+	}, []);
+
+	useEffect(() => {
+		if (windowSize > 1500) {
+			setImageNumber(4);
+		} else if (windowSize <= 1500 && windowSize > 930) {
+			setImageNumber(3);
+		} else if (windowSize <= 930 && windowSize > 760) {
+			setImageNumber(2);
+		} else if (windowSize <= 760 && windowSize > 0) {
+			setImageNumber(1);
+		}
+	}, [windowSize]);
+
+	let whirligig;
+	const next = () => whirligig.next();
+	const prev = () => whirligig.prev();
 	return (
 		<div className="carousel">
 			<div className="container">
-				<div className="leftArrow arrows ">
+				<div className="leftArrow arrows " onClick={prev}>
 					<AiOutlineLeft />
 				</div>
-				<div className="carouselBody">
+				<Whirligig
+					visibleSlides={imageNumber}
+					className="carouselBody"
+					preventScroll={true}
+					infinite={true}
+					gutter="3rem"
+					ref={(_whirligigInstance) => {
+						whirligig = _whirligigInstance;
+					}}>
 					{projects.map((project) => (
 						<img
 							className={
@@ -48,8 +82,8 @@ const Carousel = ({ currentPage }) => {
 							alt=""
 						/>
 					))}
-				</div>
-				<div className="rightArrow arrows ">
+				</Whirligig>
+				<div className="rightArrow arrows " onClick={next}>
 					<AiOutlineRight />
 				</div>
 			</div>
