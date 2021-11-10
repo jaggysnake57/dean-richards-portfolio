73de8451000365153ce32ff10dd9b8499bc3d777
Dean Richards <deanrichardsdeveloper@gmail.com>
2021-11-10 13:55:20 +0000

    add animation to projects

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,9 +1,12 @@
 import { useStateValue } from '../../contexts/ProjectsContext';
 import { db } from '../../firebase';
+import { gsap } from 'gsap';
 
 import { FiExternalLink, FiGithub } from 'react-icons/fi';
 import { BsBoxArrowLeft } from 'react-icons/bs';
 import { RiCloseFill } from 'react-icons/ri';
+import { useEffect, useRef, useState } from 'react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const ProjectCard = ({
 	isLeft,
@@ -15,8 +18,14 @@ const ProjectCard = ({
 	//context
 	const [{ isAdmin, userId }, dispatch] = useStateValue();
 
+	//state
+	const [imageLoaded, setImageLoaded] = useState(false);
+
 	// destructed props
 	const { name, imageUrl, websiteLink, gitHubLink, blurb } = project;
+
+	// hooks
+	const projectCardRef = useRef();
 
 	// functions
 	const handleDeleteProject = (id, event) => {
@@ -32,10 +41,66 @@ const ProjectCard = ({
 		}
 	};
 
+	const handleImageLoad = () => {
+		setImageLoaded(true);
+	};
+
+	//gsap
+	gsap.registerPlugin(ScrollTrigger);
+
+	useEffect(() => {
+		const startPos = isLeft ? '-100%' : '100%';
+		const finishPos = isLeft ? '-10%' : '10%';
+		if (imageLoaded) {
+			ScrollTrigger.matchMedia({
+				'(min-width: 40rem)': function () {
+					gsap.fromTo(
+						projectCardRef.current,
+						{
+							x: startPos,
+						},
+						{
+							x: finishPos,
+							duration: 2,
+							scrollTrigger: {
+								trigger: projectCardRef.current,
+
+								scrub: 1,
+								start: '0px bottom',
+								end: '0px 55%',
+							},
+						}
+					);
+				},
+				'(min-width: 2400px)': function () {
+					gsap.fromTo(
+						projectCardRef.current,
+						{
+							x: startPos,
+						},
+						{
+							x: '0px',
+							duration: 2,
+							scrollTrigger: {
+								trigger: projectCardRef.current,
+
+								scrub: 1,
+								start: '0px bottom',
+								end: '0px 55%',
+							},
+						}
+					);
+				},
+			});
+		}
+	}, [imageLoaded]);
+
 	return (
-		<div className={`project project--pull-${isLeft ? 'left' : 'right'} `}>
+		<div
+			ref={projectCardRef}
+			className={`project project--pull-${isLeft ? 'left' : 'right'} `}>
 			{isAdmin && userId && (
-				<div className="project-card__admin-buttons">
+				<div className="project__admin-buttons">
 					<button
 						className="btn"
 						onClick={() => setEditableProjectId(projectId)}>
@@ -88,7 +153,7 @@ const ProjectCard = ({
 					</div>
 				</div>
 				<div className="project__image">
-					<img src={imageUrl} alt="" />
+					<img src={imageUrl} alt="" onLoad={handleImageLoad} />
 				</div>
 			</div>
 		</div>
